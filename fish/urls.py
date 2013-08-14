08f7f2fd63a01a3fa935a6451ed6124db33f056e
Can Yilmaz <can@potatolondon.com>
2013-08-14 22:03:36 +0100

    Static config

--- fish/urls.py
+++ fish/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
-from . import views
+from . import views, settings
 
 urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT})
 )
