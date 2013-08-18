b9aef6af6d3fc4ef62105c70a504c275fb6c3ce3
Can Yilmaz <can@potatolondon.com>
2013-08-18 19:11:55 +0100

    Best project structure

--- fish/urls.py
+++ fish/urls.py
@@ -3,6 +3,5 @@ from . import views, settings
 
 urlpatterns = patterns('',
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    url(r'^static-dev/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    url(r'^$', views.home, name='home'),
+    url(r'^', views.home, name='home'),
 )
--- fish/views.py
+++ fish/views.py
@@ -1,36 +1,13 @@
-from django.views.generic.base import View, TemplateView
+from django.views.generic.base import TemplateView
 
 from . import settings
-from .models import Payment
 
-
-class PublicBaseView(View):
-    """ A base class for all views """
-
-    class Meta:
-        abstract = True
+class Home(TemplateView):
+    template_name = 'index.html'
 
     def get_context_data(self, *args, **kwargs):
-        context = super(PublicBaseView, self).get_context_data(*args, **kwargs)
+        context = super(Home, self).get_context_data(*args, **kwargs)
         context['settings'] = settings
-        return context
-
-
-class PublicBasicPageView(PublicBaseView, TemplateView):
-    """ Basic page view """
-    pass
-
-
-class Home(PublicBasicPageView):
-
-    template_name = 'home.html'
-    section_name = 'home'
-
-    def get_context_data(self):
-        context = super(Home, self).get_context_data()
-
-        context['payments'] = Payment.objects.all().order_by('-start_date')
-
         return context
 
 home = Home.as_view()
