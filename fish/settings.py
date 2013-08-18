9f4095755cafcafb51c8e1a2c6bb2aa4e1fcbaea
Can Yilmaz <can@potatolondon.com>
2013-08-18 20:25:08 +0100

    FISH!

--- fish/settings.py
+++ fish/settings.py
@@ -84,7 +84,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static-dev' if DEBUG else 'static'),
 )
 
 # List of finder classes that know how to find static files in
