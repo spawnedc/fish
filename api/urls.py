94c965c5ff825206d44e4afcead1088c041fc0a3
Can Yilmaz <can@potatolondon.com>
2013-10-25 21:47:49 +0100

    Better API definitions

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,6 @@
-from django.conf.urls.defaults import patterns, url
-from .views import api_root, PaymentList, PaymentDetail
+from .views import PaymentViewSet
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = patterns('',
-    url(r'^$', api_root),
-
-    # Payments
-    url(r'^payment$', PaymentList.as_view(), name='payment-list'),
-    url(r'^payment/(?P<pk>[0-9]+)$', PaymentDetail.as_view(), name='payment-detail'),
-
-)
+router = DefaultRouter()
+router.register(r'payment', PaymentViewSet)
+urlpatterns = router.urls
--- api/views.py
+++ api/views.py
@@ -1,29 +1,9 @@
-from rest_framework import generics
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
+from rest_framework import viewsets
 
 from fish.models import Payment
 from .serializers import PaymentSerializer
 
 
-@api_view(('GET',))
-def api_root(request, format=None):
-    return Response({
-        'Payment': reverse('payment-list', request=request),
-        # 'repository': reverse('repository-list', request=request),
-        # 'deployment': reverse('deployment-list', request=request),
-        # 'favoritebranch': reverse('favoritebranch-list', request=request),
-        # 'customenvironment': reverse('customenvironment-list', request=request),
-    })
-
-
-class PaymentList(generics.ListAPIView):
-
-    model = Payment
-    serializer_class = PaymentSerializer
-
-
-class PaymentDetail(generics.RetrieveDestroyAPIView):
-    model = Payment
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
